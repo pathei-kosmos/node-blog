89d798dcfb009175cc3962d81e5aca1fe457337c
pathei-kosmos <63155388+pathei-kosmos@users.noreply.github.com>
2022-06-12 12:46:12 +0200

    add router and compression

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const compression = require('compression');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const dbURI = require('./dbURI');
-const Article = require('./models/article');
+const articlesRoutes = require('./routes/articlesRoutes');
 const app = express();
 
 // connect to mongodb then listen for requests
@@ -21,12 +22,15 @@ mongoose.connect(dbURI)
 // --- MIDDLEWARES ---
 // define static folder
 app.use(express.static('public'));
+// compress all HTTP responses
+app.use(compression());
 // POST requests body parser
 app.use(express.urlencoded({ extended: true }));
-// secure http headers
+// secure HTTP headers
 app.use(helmet());
 // logger
 app.use(morgan('dev'));
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -36,58 +40,8 @@ app.get('/', (req, res) => {
     res.redirect('/articles');
 });
 
-// display all articles
-app.get('/articles', (req, res) => {
-    Article.find()
-        .sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { title: 'Home', articles: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-// article creation form
-app.get('/articles/create', (req, res) => {
-    res.render('createArticle.ejs', { title: 'New article' });
-});
-
-// article creation route
-app.post('/articles', (req, res) => {
-    const article = new Article(req.body);
-    article.save()
-        .then(() => {
-            res.redirect('/articles');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-// display one article
-app.get('/articles/:id', (req, res) => {
-    const id = req.params.id;
-    Article.findById(id)
-        .then(result => {
-            res.render('details', { title: 'Article Details', article: result });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-// delete one article
-app.delete('/articles/:id', (req, res) => {
-    const id = req.params.id;
-    Article.findByIdAndDelete(id)
-        .then(() => {
-            res.json({ redirect: '/articles' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+// articles routes
+app.use(articlesRoutes);
 
 // about page
 app.get('/about', (req, res) => {
